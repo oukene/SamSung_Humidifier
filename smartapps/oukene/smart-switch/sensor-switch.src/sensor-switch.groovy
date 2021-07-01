3261ebd4abe4b77ee19a671cfefa591dbcb07b5e
김상욱 <ragcarib@gmail.com>
2021-07-01 10:54:06 +0900

    - 다중 디바이스로 스위치  on / off 처리

--- smartapps/oukene/smart-switch/sensor-switch.src/sensor-switch.groovy
+++ smartapps/oukene/smart-switch/sensor-switch.src/sensor-switch.groovy
@@ -6,6 +6,9 @@
  *  Copyright 2021
  *
  */
+
+import groovy.transform.Field
+ 
 definition(
     name: "Sensor Switch",
     namespace: "oukene/smart-switch",
@@ -36,8 +39,45 @@ def dashBoardPage(){
             {
                 section() {
                     paragraph "- DashBoard", image: "https://cdn4.iconfinder.com/data/icons/finance-427/134/23-512.png"
-                    paragraph "[ $sensorType - $sensor ]"
-                    paragraph "상태: " + sensor.currentState(sensorActions(sensorType)).value
+                    paragraph "[ 스위치 켜짐 조건 ]"
+                    
+                    if(contactSensor != null) {
+                    	def isList = contactSensor instanceof List
+                        if(isList) {
+                            contactSensor.each {
+                                paragraph "" + it.displayName + " - " + (it.currentState("contact").value == contactSensorAction ? "true" : "false")
+                            }
+                        }
+                        else { paragraph "" + contactSensor.displayName + " - " + (contactSensor.currentState("contact").value == contactSensorAction ? "true" : "false")	}
+                    }
+                    if(motionSensor != null) {
+                    	def isList = motionSensor instanceof List
+                        if(isList) {
+                            motionSensor.each {
+                                paragraph "" + it.displayName + " - " + (it.currentState("motion").value == motionSensorAction ? "true" : "false")
+                            }
+                        }
+                        else { paragraph "" + motionSensor.displayName + " - " + (motionSensor.currentState("motion").value == motionSensorAction ? "true" : "false")	}
+                    }
+                    if(switches != null) {
+                    	def isList = switches instanceof List
+                        if(isList) {
+                            switches.each {
+                                paragraph "" + it.displayName + " - " + (it.currentState("switch").value == switchAction ? "true" : "false")
+                            }
+                        }
+                        else { paragraph "" + switches.displayName + " - " + (switches.currentState("motion").value == switchAction ? "true" : "false")	}
+                    }
+                    if(waterSensor != null) {
+                    	def isList = waterSensor instanceof List
+                        if(isList) {
+                            waterSensor.each {
+                                paragraph "" + it.displayName + " - " + (it.currentState("water").value == waterSensorAction ? "true" : "false")
+                            }
+                        }
+                        else { paragraph "" + waterSensor.displayName + " - " + (waterSensor.currentState("motion").value == waterSensorAction ? "true" : "false")	}
+                    }
+                    
                     paragraph "스위치 켜진시각: " + new Date(state.on_time).format('yyyy-MM-dd HH:mm:ss.SSS', location.getTimeZone())
                     paragraph "스위치 꺼진시각: " + new Date(state.off_time).format('yyyy-MM-dd HH:mm:ss.SSS', location.getTimeZone())
                     paragraph "수동모드: " + (useManualMode == true ? "사용" : "미사용")
@@ -77,35 +117,58 @@ def sensorPage()
 {
 	dynamicPage(name: "sensorPage", title: "설정", nextPage: "switchPage")
     {
+    	log.debug location.getTimeZone()
         section()
         {
+        	input(name: "contactSensor", type: "capability.contactSensor", title: "열림감지 센서", required: false, multiple: true)
+            input(name: "contactSensorAction", type: "enum", title: "다음 동작이 발생하면", options: attributeValues("contactSensor"), required: true, defaultValue: "open")
+            input(name: "motionSensor", type: "capability.motionSensor", title: "모션감지 센서", required: false, multiple: true)
+            input(name: "motionSensorAction", type: "enum", title: "다음 동작이 발생하면", options: attributeValues("motionSensor"), required: true, defaultValue: "active")
+            input(name: "switches", type: "capability.switch", title: "스위치", required: false, multiple: true)
+            input(name: "switchAction", type: "enum", title: "다음 동작이 발생하면", options: attributeValues("switch"), required: true, defaultValue: "on")
+            input(name: "waterSensor", type: "capability.waterSensor", title: "물 감지 센서", required: false, multiple: true)
+            input(name: "waterSensorAction", type: "enum", title: "다음 동작이 발생하면", options: attributeValues("waterSensor"), required: true, defaultValue: "wet")
+            
+            /*
             input "sensorType", "enum", title: "센서 선택", multiple: false, required: true, submitOnChange: true, options: [
                 "contactSensor":"열림감지 센서",
                 "motionSensor":"움직임 감지 센서",
                 "switch": "스위치",
                 "button": "버튼",
                 "waterSensor": "물 감지 센서"]
+			
+            if(sensorType != null && sensorType != "null") {
+                input(name: "sensor", type: "capability.$sensorType", title: "$sensorType 에서", required: true, multiple: false, submitOnChange: true)
+                input(name: "sensorAction", type: "enum", title: "다음 동작이 발생하면", options: attributeValues(sensorType), required: true)
+            }
+            */
         }
-        if(sensorType != null)
-        {
-            	section("$sensorType 설정") {
-                    input(name: "sensor", type: "capability.$sensorType", title: "$sensorType 에서", required: true)
-                    input(name: "sensorAction", type: "enum", title: "다음 동작이 발생하면", options: attributeValues(sensorType), required: true)
-                }
+        section() {
+        	input "isAllDevices", "bool", title: "선택된 디바이스 모두에서 조건 만족 할 경우에만 켜짐", required: true, defaultValue: false
         }
     }
 }
 
 def switchPage()
-{
+{    
     dynamicPage(name: "switchPage", title: "", nextPage: "optionPage")
     {
-        section("스위치 설정") {
-            input(name: "main_switch", type: "capability.switch", title: "이 스위치와", required: true, submitOnChange: true)
-            input(name: "sub_switch", type: "capability.switch", title: "추가로 이 스위치들을", multiple: true, required: false)
-            if(main_switch)
-            {
-            	input(name: "reactionValue", type: "enum", title: "아래와 같이 실행하고 조건이 변경되면 끕니다(버튼은 끄기 제외)", options: reactionValues("switch"), required: true)
+    	if(null == contactSensor && null == motionSensor &&  null == switchAction && null == waterSensor)
+        {
+        	section("조건으로 작동할 디바이스가 선택되지 않았습니다") {
+                href "sensorPage", title: "설정", description:"", image: "https://cdn4.iconfinder.com/data/icons/industrial-1-4/48/33-512.png"
+            }
+		}
+        else
+        {
+        	log.debug "is All Device: " + isAllDevices
+            section("스위치 설정") {
+                input(name: "main_switch", type: "capability.switch", title: "이 스위치와", required: true)
+                input(name: "sub_switch", type: "capability.switch", title: "추가로 이 스위치들을 켭니다", multiple: true, required: false)
+                if(main_switch)
+                {
+                    input "isAllDevices_off", "bool", title: "선택된 디바이스 모두에서 조건이 만족되지 않을 경우에만 꺼짐", required: true, defaultValue: true
+                }
             }
         }
     }
@@ -123,19 +186,16 @@ def optionPage()
     dynamicPage(name: "optionPage", title: "")
     {
         section("그리고 아래 옵션을 적용합니다(미 설정시 적용되지 않음)") {
-        	if(reactionValue == "on")
+            input "light_meter", "capability.illuminanceMeasurement", title: "조도 센서", required: false, multiple: false, submitOnChange: true
+            if(light_meter)
             {
-                input "light_meter", "capability.illuminanceMeasurement", title: "조도 센서", required: false, multiple: false, submitOnChange: true
-                if(light_meter)
-                {
-                    input "lux_max", "number", title: "기준 조도값", required: true, 
-                        defaultValue: "30"
-                }
-                // 이 옵션들은 스위치를 켤때만 적용
-                input "stay", "number", required: true, title: "동작 조건 변경 후 꺼짐지연시간(초)", defaultValue: "0"
-                input "useManualMode", "bool", title: "수동모드 활성화", defaultValue: false, required: true
-			}
-            input "enableLog", "bool", title: "로그활성화", required: true, defaultValue: false
+                input "lux_max", "number", title: "기준 조도값", required: true, 
+                    defaultValue: "30"
+            }
+            // 이 옵션들은 스위치를 켤때만 적용
+            input "stay", "number", required: true, title: "동작 조건 변경 후 꺼짐지연시간(초)", defaultValue: "0"
+            input "useManualMode", "bool", title: "수동모드 활성화", defaultValue: true, required: true
+            input "enableLog", "bool", title: "로그활성화", required: true, defaultValue: true
         }
         timeInputs()
         
@@ -146,7 +206,7 @@ def optionPage()
         
         if (!overrideLabel) {
             // if the user selects to not change the label, give a default label
-            def l = sensor.displayName + "-" + main_switch.displayName + ": Sensor Switch"
+            def l = main_switch.displayName + ": Sensor Switch"
             log.debug "will set default label of $l"
             app.updateLabel(l)
         }
@@ -191,6 +251,19 @@ private attributeValues(attributeName) {
     }
 }
 
+private getDeviceAction(name) {
+	switch(name) {
+    	case "motion":
+        	return motionSensorAction
+		case "contact":
+        	return contactSensorAction
+		case "switch":
+        	return switchAction
+		case "water":
+        	return waterSensorAction
+    }
+}
+
 private sensorActions(name) {
     switch(name) {
         case "switch":
@@ -210,7 +283,7 @@ private sensorActions(name) {
 
 def uninstalled() {
 	log("uninstall")
-    deleteDevice()
+    //deleteDevice()
 }
 
 def installed() {
@@ -233,9 +306,16 @@ def initialize()
         app.updateLabel(app.label)
     }
     
-    def action = sensorActions(sensorType)
-    log("sensor: $sensor , action : $action , reactionValue : $reactionValue")
-	subscribe(sensor, sensorActions(sensorType), eventHandler)
+    // 각각의 디바이스 타입에 따라 subscribe
+    subscribe(contactSensor, "contact", eventHandler)
+    subscribe(motionSensor, "motion", eventHandler)
+    subscribe(switches, "switch", eventHandler)
+    subscribe(waterSensor, "water", eventHandler)
+    
+    //def action = sensorActions(sensorType)
+    //log("sensor: $sensor , action : $action , reactionValue : $reactionValue")
+	//subscribe(sensorList, sensorActions(sensorType), eventHandler)
+    //subscribe(sensorList[0], sensorActions("motionSensor"), eventHandler)
     
     //if(light_meter != null)
     //{
@@ -256,19 +336,79 @@ def initialize()
     log("init finish")
 }
 
-def eventHandler(evt)
+def isAllDevicesConditionCheck(forOff)
 {
-	log("$evt.name : $evt.value : $sensorAction : $reactionValue")
+	def ret = true
     
-    state.lastEventValue = evt.value
-	if (evt.value == sensorAction && reactionValue == "on")
+    if(contactSensor) {
+		contactSensor.each {
+        	if(forOff) {
+            	if(it.currentState("contact").value == contactSensorAction)
+                    ret = false
+            }
+            else {
+                if(it.currentState("contact").value != contactSensorAction)
+                    ret = false
+			}
+        }
+	}
+    if(motionSensor) {
+    	motionSensor.each {
+        	if(forOff) {
+            	if(it.currentState("motion").value == motionSensorAction)
+            		ret = false
+            }
+            else {
+            	if(it.currentState("motion").value != motionSensorAction)
+            		ret = false
+			}
+        }
+    }
+    if(switches) {
+    	switches.each {
+        	if(forOff) {
+            	if(it.currentState("switch").value == switchAction)
+            		ret = false
+            }
+            else {
+            	if(it.currentState("switch").value != switchAction)
+            		ret = false
+            }
+        	
+        }
+    }
+    if(waterSensor) {
+    	waterSensor.each {
+        	if(forOff) {
+            	if(it.currentState("water").value == waterSensorAction)
+            		ret = false
+            }
+            else {
+            	if(it.currentState("water").value != waterSensorAction)
+            		ret = false
+            }
+        }
+    }
+    return ret
+}
+
+def eventHandler(evt)
+{    
+    def triggerCondition = getDeviceAction(evt.name)
+    
+    log("$evt.name : $evt.value : $triggerCondition")
+        
+	if (evt.value == triggerCondition)
     {
-        unschedule(switchOff)
+    	if(isAllDevices && !isAllDevicesConditionCheck(false)) return
+        
     	def isBetween = true
         if(null != startTime && null != endTime) { isBetween = timeOfDayIsBetween(startTime, endTime, new Date(), location.timeZone) }
         log("between: $isBetween")
     	if(isBetween)
         {
+        	unschedule(switchOff)
+            
         	log("main switch: " + main_switch.currentState("switch").value)
             log("light_meter: " + light_meter)
 			// 켜짐 작동
@@ -276,51 +416,33 @@ def eventHandler(evt)
                (light_meter == null || (light_meter != null && light_meter.currentIlluminance <= lux_max)))
             {
                 state.autoMode = true
+                log("set AutoMode")
                 switchOn()
             }
             log("main switch on")
         }
     }
-    else if(evt.value == sensorAction && reactionValue == "toggle")
+    else if(evt.value != triggerCondition)
     {
-    	def isBetween = true
-        if(null != startTime && null != endTime) { isBetween = timeOfDayIsBetween(startTime, endTime, new Date(), location.timeZone) }
-        log("between: $isBetween")
-    	if(isBetween)
+        if(state.autoMode == true)
         {
-        	main_switch.currentSwitch == "on" ? switchOff() : switchOn()
+        	log.debug "isAllDeviceOff: $isAllDevices_off"
+            log.debug "check result: " + isAllDevicesConditionCheck()
+            //꺼짐 작동
+            if(isAllDevices_off && !isAllDevicesConditionCheck(true)) return
+			
+            log("scheduled off : $stay seconds")
+            if(0 == stay) runIn(stay, switchOff, [overwrite: true])
+            else schedule(now() + (stay *1000), switchOff)
+            //runIn(stay, switchOff, [overwrite: true])
         }
-    }
-    else if(evt.value == sensorAction && reactionValue == "off")
-    {
-    	switchOff()
-    }
-    else if(evt.value != sensorAction)
-    {
-    	if(reactionValue == "on")
-        {
-            if(state.autoMode == true)
-            {
-            	if(sensorType != "button")
-                {
-                    //꺼짐 작동
-                    log("scheduled off : $stay seconds")
-                    if(0 == stay) runIn(stay, switchOff, [overwrite: true])
-                    else schedule(now() + (stay *1000), switchOff)
-                    //runIn(stay, switchOff, [overwrite: true])
-				}
-            }
-		}
-        /*
-        else if(reactionValue == "off")
-        {
-        	
-        }
-        */
     }
 }
 
 def switchOff() {
+	if(true == useManualMode && false == state.autoMode) 
+    	return
+    
     log("switchOff")
     main_switch.off()
     if(null != sub_switch)
